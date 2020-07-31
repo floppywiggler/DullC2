1855f2682b8e8a736278457a38e9112c4f52ebe3
Emil <esorbroden@gmail.com>
2020-07-31 11:33:40 +0200

    ClientAuthentication

--- DullC2/DullC2/Controllers/ClientController.cs
+++ DullC2/DullC2/Controllers/ClientController.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TeamServer.Models;
 
 namespace TeamServer.Controllers
 {
     public class ClientController
     {
+       private List<string> _connectedClients { get; set; }
+
+        public ClientAuthenticationResult ClientLogin(ClientAuthenticationRequest request)
+        {
+            var result = new ClientAuthenticationResult();
+            if (string.IsNullOrEmpty(request.Nick) || string.IsNullOrEmpty(request.Password))
+            {
+                result.Result = ClientAuthenticationResult.AuthResult.InvalidRequest;
+            }
+            else if (!AuthenticationController.ValidatePassword(request.Password))
+            {
+                result.Result = ClientAuthenticationResult.AuthResult.BadPassword;
+            }
+            else if (_connectedClients.Contains(request.Nick, StringComparer.OrdinalIgnoreCase))
+            {
+                result.Result = ClientAuthenticationResult.AuthResult.NickInUse;
+            }
+            else
+            {
+                result.Result = ClientAuthenticationResult.AuthResult.LoginSuccess;
+            }
+            return result;
+        }
+
     }
 }
